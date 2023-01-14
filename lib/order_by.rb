17f4ead09d087cb514507a26cafc61650fefbe1a
Ian Heraty <ian.heraty@pm.me>
2023-01-14 00:40:11 -0600

    Hotfix for handling nil in order_by

--- lib/order_by.rb
+++ lib/order_by.rb
@@ -7,7 +7,7 @@ module Orderable
     # @param nulls [String] NULLS FIRST | NULLS LAST
     # @return [ActiveRecord::Relation] ordered active record relation
     def order_by(column, direction = nil, nulls = nil)
-      sort_string = [column.underscore, direction.upcase, nulls.upcase].reject(&:blank?).join(' ')
+      sort_string = [column&.underscore, direction&.upcase, nulls&.upcase].reject(&:blank?).join(' ')
       order(Arel.sql(sort_string))
     end
   end
